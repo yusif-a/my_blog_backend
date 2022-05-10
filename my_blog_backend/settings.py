c1c1da3e3c334c5dc5708352a828ad507b7d0d80
yusif-a <y.ajoodani@gmail.com>
2022-05-10 17:42:07 +0430

    default auth set to token based

--- my_blog_backend/settings.py
+++ my_blog_backend/settings.py
@@ -121,6 +121,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
