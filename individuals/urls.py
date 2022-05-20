3f008c3b37b35e1e90e6a4b5c63a30ccf1b5b4df
yusif-a <y.ajoodani@gmail.com>
2022-05-20 23:14:24 +0430

    permission viewset url added.

--- individuals/urls.py
+++ individuals/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserViewSet, GroupViewSet
+from .views import UserViewSet, GroupViewSet, PermissionViewSet
 from .config import APP_NAME
 
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'groups', GroupViewSet, basename='group')
+router.register(r'permissions', PermissionViewSet, basename='permission')
 
 app_name = APP_NAME
 urlpatterns = [
